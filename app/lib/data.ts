25dcf5fa659bb11b179cce7cc5e8cca37d6f591b
vitaliy_m <miskotop1234@gmail.com>
2024-08-01 20:48:23 +0300

    done work

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -158,6 +158,7 @@ export async function fetchInvoiceById(id: string) {
       amount: invoice.amount / 100,
     }));
 
+
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
